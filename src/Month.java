f8afab1f987f73d3cdafd1c4ce30c2b7c87c5f1e
Karrex <e.chernihovsky@gmail.com>
2015-04-20 18:34:18 +0300

    Month v. 3.0

--- src/Month.java
+++ src/Month.java
@@ -1,23 +1,23 @@
-public class Month {
-    enum Year {
-        JAN(31), FEB(28), MAR(31), APR(30), MAY(31), JUN(30), JUL(31), AUG(31), SEP(30),
-        OCT(31), NOV(30), DEC(31);
-        private int day;
+enum Year {
+    JAN(31), FEB(28), MAR(31), APR(30), MAY(31), JUN(30), JUL(31), AUG(31), SEP(30),
+    OCT(31), NOV(30), DEC(31);
+    private int day;
 
-        Year(int day) {
-            this.day = day;
-        }
-
-        public int getDay() {
-            return day;
-        }
-
-        public static void hello(Year year) {
-            System.out.println("Hello, It's " + year + ", month number - " + (year.ordinal() + 1) + ". In " + year +
-                    " " + year.getDay() + " day,");
-        }
+    Year(int day) {
+        this.day = day;
     }
 
+    public int getDay() {
+        return day;
+    }
+
+    public static void hello(Year year) {
+        System.out.println("Hello, It's " + year + ", month number - " + (year.ordinal() + 1) + ". In " + year +
+                " " + year.getDay() + " day,");
+    }
+}
+
+public class Month {
     public static void main(String[] args) {
         Year year = Year.valueOf(args[0].substring(0, 3).toUpperCase());
         Year.hello(year);
